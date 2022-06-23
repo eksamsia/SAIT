26932411a8cd3f850f0507ee326ec18d0558d366
eksamsia <76504562+eksamsia@users.noreply.github.com>
2022-06-23 19:23:11 +0700

    database on ubuntu

--- akademik.sql
+++ akademik.sql
@@ -1,92 +1,83 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 23, 2022 at 02:02 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.23
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: akademik
+-- ------------------------------------------------------
+-- Server version	8.0.29-0ubuntu0.21.10.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `akademik`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `mahasiswa2`
 --
 
+DROP TABLE IF EXISTS `mahasiswa2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mahasiswa2` (
-  `id_mhs` int(11) NOT NULL,
+  `id_mhs` int NOT NULL AUTO_INCREMENT,
   `nama` varchar(100) DEFAULT NULL,
   `alamat` varchar(100) DEFAULT NULL,
-  `prodi` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `prodi` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_mhs`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `mahasiswa2`
+--
+
+LOCK TABLES `mahasiswa2` WRITE;
+/*!40000 ALTER TABLE `mahasiswa2` DISABLE KEYS */;
+INSERT INTO `mahasiswa2` VALUES (1,'Fajar','Undaan','TRPL'),(3,'Tyas','Bantul','TRPL');
+/*!40000 ALTER TABLE `mahasiswa2` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `taylor`
 --
 
+DROP TABLE IF EXISTS `taylor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `taylor` (
-  `id` int(100) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `judul` varchar(100) DEFAULT NULL,
   `album` varchar(100) DEFAULT NULL,
-  `tahun` int(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `tahun` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `taylor`
 --
 
-INSERT INTO `taylor` (`id`, `judul`, `album`, `tahun`) VALUES
-(7, 'Speak now', 'Speak now', 2010);
+LOCK TABLES `taylor` WRITE;
+/*!40000 ALTER TABLE `taylor` DISABLE KEYS */;
+INSERT INTO `taylor` VALUES (7,'Speak now','Speak now',2010);
+/*!40000 ALTER TABLE `taylor` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for dumped tables
+-- Dumping routines for database 'akademik'
 --
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `mahasiswa2`
---
-ALTER TABLE `mahasiswa2`
-  ADD PRIMARY KEY (`id_mhs`);
-
---
--- Indexes for table `taylor`
---
-ALTER TABLE `taylor`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `mahasiswa2`
---
-ALTER TABLE `mahasiswa2`
-  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `taylor`
---
-ALTER TABLE `taylor`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-23 19:21:35
